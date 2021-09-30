d270793312fcbfd6baa6b43e44c0bb71d990a4cf
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-09-30 11:44:39 +0100

    Request data from URL on `load`

--- Swapp/SwappTests/RemoteStarWarsLoaderTests.swift
+++ Swapp/SwappTests/RemoteStarWarsLoaderTests.swift
@@ -8,7 +8,17 @@
 import Foundation
 import XCTest
 
-class RemoteStarWarsLoader {}
+class RemoteStarWarsLoader {
+    private let client: HTTPClient
+
+    init(client: HTTPClient) {
+        self.client = client
+    }
+
+    func load() {
+        client.requestedURL = URL(string: "https://a-url.com")
+    }
+}
 
 class HTTPClient {
     var requestedURL: URL?
@@ -17,8 +27,17 @@ class HTTPClient {
 class RemoteStarWarsLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClient()
-        _ = RemoteStarWarsLoader()
+        _ = RemoteStarWarsLoader(client: client)
 
         XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient()
+        let sut = RemoteStarWarsLoader(client: client)
+
+        sut.load()
+
+        XCTAssertNotNil(client.requestedURL)
     }
 }
