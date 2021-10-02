377b0975d0d052a5c2c0368ca4cc4d16ff247e8f
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-10-02 12:05:43 +0100

    Enable code coverage gathering Enable random test execution order to guarantee there is no order dependency between tests methods Refactor code.

--- Swapp/SwappTests/StarWars API/URLSessionHTTPClientTests.swift
+++ Swapp/SwappTests/StarWars API/URLSessionHTTPClientTests.swift
@@ -145,8 +145,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
 
         override class func canInit(with request: URLRequest) -> Bool {
-            requestObserver?(request)
-            return true
+            true
         }
 
         override class func canonicalRequest(for request: URLRequest) -> URLRequest {
@@ -154,6 +153,11 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
 
         override func startLoading() {
+            if let requestObserver = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                return requestObserver(request)
+            }
+
             if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
