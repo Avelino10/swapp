aba028ef00b28242a4b1a8c85ea5eb0711a771cf
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-10-05 20:39:00 +0100

    - `RemoteStarWarsImageDataLoader` requests data from URL twice on calling `loadImageDataFromURL` twice - `RemoteStarWarsImageDataLoader.loadImageDataFromURL` delivers error on client error

--- Swapp/SwappTests/StarWars API/RemoteStarWarsImageDataLoaderTests.swift
+++ Swapp/SwappTests/StarWars API/RemoteStarWarsImageDataLoaderTests.swift
@@ -14,8 +14,15 @@ class RemoteStarWarsImageDataLoader {
         self.client = client
     }
 
-    func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
-        client.get(from: url) { _ in }
+    func loadImageData(from url: URL, completion: @escaping (StarWarsImageDataLoader.Result) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+                case let .failure(error):
+                    completion(.failure(error))
+                default:
+                    break
+            }
+        }
     }
 }
 
@@ -28,19 +35,66 @@ class RemoteStarWarsImageDataLoaderTests: XCTestCase {
 
     func test_loadImageDataFromURL_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
+        let (sut, client) = makeSUT()
 
         sut.loadImageData(from: url) { _ in }
 
         XCTAssertEqual(client.requestedURLs, [url])
     }
 
-    private func makeSUT(url: URL = URL(string: "http://a-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteStarWarsImageDataLoader, client: HTTPClientSpy) {
+    func test_loadImageDataFromURLTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT()
+
+        sut.loadImageData(from: url) { _ in }
+        sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+
+    func test_loadImageDataFromURL_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        let clientError = NSError(domain: "a client error", code: 0)
+
+        expect(sut, toCompleteWith: .failure(clientError), when: {
+            client.complete(with: clientError)
+        })
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: RemoteStarWarsImageDataLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteStarWarsImageDataLoader(client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
 
         return (sut, client)
+    }
+
+    private func anyData() -> Data {
+        Data("any data".utf8)
+    }
+
+    private func expect(_ sut: RemoteStarWarsImageDataLoader, toCompleteWith expectedResult: StarWarsImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let url = URL(string: "https://a-given-url.com")!
+        let exp = expectation(description: "Wait for load completion")
+
+        sut.loadImageData(from: url) { receivedResult in
+            switch (receivedResult, expectedResult) {
+                case let (.success(receivedData), .success(expectedData)):
+                    XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+                case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                    XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                default:
+                    XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+
+            exp.fulfill()
+        }
+
+        action()
+
+        wait(for: [exp], timeout: 1.0)
     }
 }
